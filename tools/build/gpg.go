4ab5deadda1ade8d37b2d6d74dd1f587226ded6f
Darien Raymond <admin@v2ray.com>
2017-08-28 23:35:50 +0200

    loopback pinentry mode

--- tools/build/gpg.go
+++ tools/build/gpg.go
@@ -7,9 +7,9 @@ import (
 )
 
 func GPGSignFile(file string, passphrase string) error {
-	cmd := exec.Command("gpg", "--digest-algo", "SHA512", "--passphrase-fd", "0", "--output", file+".sig", "--detach-sig", file)
+	cmd := exec.Command("gpg", "--pinentry-mode", "loopback", "--digest-algo", "SHA512", "--passphrase-fd", "0", "--output", file+".sig", "--detach-sig", file)
 	cmd.Env = append(cmd.Env, os.Environ()...)
-	cmd.Stdin = strings.NewReader(passphrase)
+	cmd.Stdin = strings.NewReader(passphrase+ "\n\n")
 	output, err := cmd.CombinedOutput()
 	if len(output) > 0 {
 		os.Stdout.Write(output)
