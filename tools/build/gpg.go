1b0ba0fe35ad451aa01e13ffa2baf2ef29ec05af
Darien Raymond <admin@v2ray.com>
2017-08-28 22:48:39 +0200

    fix gpg passphrase

--- tools/build/gpg.go
+++ tools/build/gpg.go
@@ -1,13 +1,15 @@
 package build
 
 import (
+	"strings"
 	"os"
 	"os/exec"
 )
 
 func GPGSignFile(file string, passphrase string) error {
-	cmd := exec.Command("gpg", "--digest-algo", "SHA512", "--no-tty", "--batch", "--passphrase", passphrase, "--output", file+".sig", "--detach-sig", file)
+	cmd := exec.Command("gpg", "--digest-algo", "SHA512", "--passphrase-fd", "0", "--output", file+".sig", "--detach-sig", file)
 	cmd.Env = append(cmd.Env, os.Environ()...)
+	cmd.Stdin = strings.NewReader(passphrase)
 	output, err := cmd.CombinedOutput()
 	if len(output) > 0 {
 		os.Stdout.Write(output)
