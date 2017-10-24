a26ddd4d6aad4440bd0d5e60528a33ee4d68e791
Darien Raymond <admin@v2ray.com>
2017-10-24 23:21:59 +0200

    update command

--- tools/build/7z.go
+++ tools/build/7z.go
@@ -10,7 +10,7 @@ func SevenZipBuild(folder string, targetFile string, password string) error {
 		"a", "-p" + password, "-mhe=on", targetFile, folder,
 	}
 
-	cmd := exec.Command("go", args...)
+	cmd := exec.Command("7z", args...)
 	cmd.Env = append(cmd.Env, os.Environ()...)
 	output, err := cmd.CombinedOutput()
 	if len(output) > 0 {
