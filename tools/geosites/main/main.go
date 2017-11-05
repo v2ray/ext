fd23634fe1d7c4566d37de4eef21afbcfb45fe93
Darien Raymond <admin@v2ray.com>
2017-11-05 17:19:19 +0100

    geosite gen

---
+++ tools/geosites/main/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gogo/protobuf/proto"
+	"v2ray.com/core/app/router"
+	"v2ray.com/ext/tools/geosites"
+)
+
+func main() {
+	geoSiteList := new(router.GeoSiteList)
+	geoSiteList.Entry = append(geoSiteList.Entry, &router.GeoSite{
+		CountryCode: "CN",
+		Domain:      geosites.GetGeoSiteCN(),
+	})
+
+	geoSiteListBytes, err := proto.Marshal(geoSiteList)
+	if err != nil {
+		fmt.Println("failed to marshal geosites:", err)
+		return
+	}
+	if err := ioutil.WriteFile("geosite.dat", geoSiteListBytes, 0777); err != nil {
+		fmt.Println("failed to write geosite.dat.", err)
+	}
+}
