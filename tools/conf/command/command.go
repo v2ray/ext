26452725813d1159d916befcd4ac2aa448973f25
Darien Raymond <admin@v2ray.com>
2017-11-12 09:56:57 +0100

    exit with error code

--- tools/conf/command/command.go
+++ tools/conf/command/command.go
@@ -15,18 +15,18 @@ func init() {
 		pbConfig, err := serial.LoadJSONConfig(os.Stdin)
 		if err != nil {
 			os.Stderr.WriteString("failed to parse json config: " + err.Error())
-			return
+			os.Exit(-1)
 		}
 
 		bytesConfig, err := proto.Marshal(pbConfig)
 		if err != nil {
 			os.Stderr.WriteString("failed to marshal proto config: " + err.Error())
-			return
+			os.Exit(-1)
 		}
 
 		if _, err := os.Stdout.Write(bytesConfig); err != nil {
 			os.Stderr.WriteString("failed to write proto config: " + err.Error())
-			return
+			os.Exit(-1)
 		}
 	}))
 }
