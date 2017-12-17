fd812213898bffc50c16d827ce20756450954a8c
Darien Raymond <admin@v2ray.com>
2017-12-18 00:08:00 +0100

    HasSuffix

--- assert/string.go
+++ assert/string.go
@@ -7,3 +7,7 @@ import (
 var HasSubstring = CreateMatcher(func(a, b string) bool {
 	return strings.Contains(a, b)
 }, "contains substring")
+
+var HasSuffix = CreateMatcher(func(a, b string) bool {
+	return strings.HasSuffix(a, b)
+}, "has suffix")
