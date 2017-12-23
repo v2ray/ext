db73399daca45f95c4e8f65c3be711e6c565155d
Darien Raymond <admin@v2ray.com>
2017-12-23 23:45:38 +0100

    new protogen

--- ext.go
+++ ext.go
@@ -1,1 +1,6 @@
 package ext
+
+//go:generate go get -u "github.com/golang/protobuf/protoc-gen-go"
+//go:generate go get -u "github.com/golang/protobuf/proto"
+//go:generate go install "v2ray.com/ext/tools/vprotogen"
+//go:generate vprotogen -repo v2ray.com/ext
