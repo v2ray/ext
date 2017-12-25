0f367ecb2954ff9708a6945c217930ef59540d9d
Darien Raymond <admin@v2ray.com>
2017-12-26 00:33:57 +0100

    include local ip in geoip

--- tools/geoip/main/main.go
+++ tools/geoip/main/main.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"v2ray.com/core/app/router"
+	"v2ray.com/core/common"
 	"v2ray.com/ext/tools/conf"
 )
 
@@ -101,6 +102,7 @@ func main() {
 			Cidr:        cidr,
 		})
 	}
+	geoIPList.Entry = append(geoIPList.Entry, getLocalIPs())
 
 	geoIPBytes, err := proto.Marshal(geoIPList)
 	if err != nil {
@@ -109,5 +111,38 @@ func main() {
 
 	if err := ioutil.WriteFile("geoip.dat", geoIPBytes, 0777); err != nil {
 		fmt.Println("Error writing geoip to file:", err)
+	}
+}
+
+var (
+	localIPs = []string{
+		"0.0.0.0/8",
+		"10.0.0.0/8",
+		"100.64.0.0/10",
+		"127.0.0.0/8",
+		"169.254.0.0/16",
+		"172.16.0.0/12",
+		"192.0.0.0/24",
+		"192.0.2.0/24",
+		"192.168.0.0/16",
+		"198.18.0.0/15",
+		"198.51.100.0/24",
+		"203.0.113.0/24",
+		"::1/128",
+		"fc00::/7",
+		"fe80::/10",
+	}
+)
+
+func getLocalIPs() *router.GeoIP {
+	cidr := make([]*router.CIDR, 0, 16)
+	for _, ip := range localIPs {
+		c, err := conf.ParseIP(ip)
+		common.Must(err)
+		cidr = append(cidr, c)
+	}
+	return &router.GeoIP{
+		CountryCode: "PRIVATE",
+		Cidr:        cidr,
 	}
 }
