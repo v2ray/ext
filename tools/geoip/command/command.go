da5036c08ce34efbd917d7de67baaa8abc07ee73
Darien Raymond <admin@v2ray.com>
2018-04-09 16:58:19 +0200

    temporarily remove geoip command

--- tools/geoip/command/command.go
+++ tools/geoip/command/command.go
@@ -1,17 +1,4 @@
 package command
 
-import (
-	"fmt"
-
-	"v2ray.com/core/common"
-	"v2ray.com/ext/tools/control"
-)
-
 func init() {
-	const name = "geoip"
-	common.Must(control.RegisterCommand(name, "Control over GeoIP data.", func(args []string) {
-		if len(args) == 0 {
-			fmt.Println()
-		}
-	}))
 }
