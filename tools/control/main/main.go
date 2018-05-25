17802d14a5aeb711e4208c02014ca910c9dbfbbe
Darien Raymond <admin@v2ray.com>
2018-05-26 00:13:57 +0200

    exit with negative code if error

--- tools/control/main/main.go
+++ tools/control/main/main.go
@@ -30,13 +30,19 @@ func main() {
 	}
 
 	if err := cmd.Execute(os.Args[2:]); err != nil {
+		hasError := false
 		if err != flag.ErrHelp {
 			fmt.Fprintln(os.Stderr, err.Error())
 			fmt.Fprintln(os.Stderr)
+			hasError = true
 		}
 
 		for _, line := range cmd.Description().Usage {
 			fmt.Println(line)
+		}
+
+		if hasError {
+			os.Exit(-1)
 		}
 	}
 }
