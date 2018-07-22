66bb2a7cbf58b4dda367e08fae6251c5dfbd95f7
Darien Raymond <admin@v2ray.com>
2018-07-22 10:38:09 +0200

    update generate files

--- tools/conf/command/errors.generated.go
+++ tools/conf/command/errors.generated.go
@@ -2,4 +2,6 @@ package command
 
 import "v2ray.com/core/common/errors"
 
-func newError(values ...interface{}) *errors.Error { return errors.New(values...).Path("Ext", "Tools", "Conf", "Command") }
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).Path("Ext", "Tools", "Conf", "Command")
+}
--- tools/control/errors.generated.go
+++ tools/control/errors.generated.go
@@ -3,5 +3,5 @@ package control
 import "v2ray.com/core/common/errors"
 
 func newError(values ...interface{}) *errors.Error {
-	return errors.New(values...).Path("Proxy", "Shadowsocks")
+	return errors.New(values...).Path("Ext", "Tools", "Control")
 }
