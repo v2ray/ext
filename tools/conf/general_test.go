9bea52176556eec3b744945b0d74347072c6cde5
Darien Raymond <admin@v2ray.com>
2018-09-19 13:09:36 +0200

    more test cases

--- tools/conf/general_test.go
+++ tools/conf/general_test.go
@@ -2,21 +2,12 @@ package conf_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
 	"v2ray.com/core/common"
 	. "v2ray.com/ext/tools/conf"
 )
-
-func diffMessage(a proto.Message, b proto.Message) error {
-	if !proto.Equal(a, b) {
-		return fmt.Errorf("Difference in messages\n%v\n%v", a, b)
-	}
-
-	return nil
-}
 
 func loadJSON(creator func() Buildable) func(string) (proto.Message, error) {
 	return func(s string) (proto.Message, error) {
